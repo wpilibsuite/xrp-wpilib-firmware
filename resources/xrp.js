008b41f1f35138059278eb41c616546fe9c94c49
Joe Pokorny <jpokornyiii@gmail.com>
2024-01-03 18:15:34 -0500

    Adding WPA complliant checks in Web UI

--- resources/xrp.js
+++ resources/xrp.js
@@ -23,9 +23,24 @@ window.onload = () => {
     saveButton.onclick = (e) => {
         e.preventDefault();
         console.log("Save Button Clicked");
-    
         try {
-            JSON.parse(configJsonEntry.value);
+            console.log(configJsonEntry.value.toString());
+
+            // Check if can parse
+            let jsonObj = JSON.parse(configJsonEntry.value);
+            
+            // Check if password 8 characters or more
+            let password = jsonObj["network"]["defaultAP"]["password"];
+            if(password.length < 8 || password.length > 63) {
+                throw new Error("Default AP password must be at least 8 characters and less than 64 per WPA standards");
+            }
+
+            // Don't allow for escape characters
+            if(configJsonEntry.value.toString().includes("\\")) {
+                throw new Error("Cannot use escape character '\\'.");
+            }
+
+            // Save config back to XRP disk
             fetch("/saveconfig", {
                 body: configJsonEntry.value,
                 headers: {
@@ -35,10 +50,11 @@ window.onload = () => {
             })
             .then(() => {
                 alert("Configuration Updated. Please reset the XRP");
-            })
+            });
         }
         catch (e) {
-            alert("Invalid JSON. Please check and try again");
+            console.log("Invalid JSON. Please check and try again. Error: " + e);
+            alert("Invalid JSON. Please check and try again.\n" + e);
         }
     }
     
